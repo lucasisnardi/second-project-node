9dcdf39232cd3564853672b8242d4c69594e25ed
lucasisnardi <lucasisnardi08@gmail.com>
2024-03-05 16:23:11 -0300

    finished my second project node

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const uuid = require('uuid');
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+
+app.get('/users', (request, response) => {
+    return response.json(users);
+});
+
+app.post('/users', (request, response) => {
+    const { order, clientName, price } = request.body;
+    const user = { id: uuid.v4(), order, clientName, price, status: "Em preparação" };
+    users.push(user);
+    return response.status(201).json(user);
+});
+
+app.put('/users/:id', (request, response) => {
+    const { id } = request.params;
+    const { order, clientName, price } = request.body;
+    const index = users.findIndex(user => user.id === id);
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" });
+    }
+    const updatedUser = { id, order, clientName, price, status: users[index].status };
+    users[index] = updatedUser;
+    return response.json(updatedUser);
+});
+
+app.delete('/users/:id', (request, response) => {
+    const { id } = request.params;
+    const index = users.findIndex(user => user.id === id);
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" });
+    }
+    users.splice(index, 1);
+    return response.status(204).json();
+});
+
+
+app.patch('/order/:id', (request, response) => {
+    const { id } = request.params;
+    const index = users.findIndex(user => user.id === id);
+    if (index < 0) {
+        return response.status(404).json({ message: "Order not found" });
+    }
+    
+    if (users[index].status !== "Em preparação") {
+        return response.status(400).json({ message: "Pedido não está em preparação" });
+    }
+    
+    users[index].status = "Pronto";
+    return response.json(users[index]);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
