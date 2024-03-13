fb5153c2fa6c779cbb9fd431de0f4ea4a7ba685e
lucasisnardi <lucasisnardi08@gmail.com>
2024-03-13 19:55:24 -0300

    change

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require ('express');
+const express = require('express');
 const uuid = require('uuid');
 const cors = require('cors');
 
 const port = 3001;
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const users = []    
-
+const users = [];
 
 app.get('/users', (request, response) => {
     return response.json(users);
 });
 
 app.post('/users', (request, response) => {
-    const { order, clientName} = request.body;
-    const user = { id: uuid.v4(), order, clientName, price, status: "Em preparação" };
+    const { order, clientName, price } = request.body; 
+    const user = { id: uuid.v4(), order, clientName, price, status: "Em preparação" }; 
     users.push(user);
     return response.status(201).json(user);
 });
@@ -43,22 +42,21 @@ app.delete('/users/:id', (request, response) => {
     return response.status(204).json();
 });
 
-
 app.patch('/order/:id', (request, response) => {
     const { id } = request.params;
     const index = users.findIndex(user => user.id === id);
     if (index < 0) {
         return response.status(404).json({ message: "Order not found" });
     }
-    
+
     if (users[index].status !== "Em preparação") {
         return response.status(400).json({ message: "Pedido não está em preparação" });
     }
-    
+
     users[index].status = "Pronto";
     return response.json(users[index]);
 });
-
+    
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
